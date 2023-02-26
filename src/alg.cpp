1ffc6585111c53248842e181486d82c6e66c660c
olesyakomarova <116936462+olesyakomarova@users.noreply.github.com>
2023-02-26 15:24:43 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,7 +4,7 @@
 
 
 bool checkPrime(uint64_t value) {
-if (value <= 1)
+if (value <= 1){
         return false;
     for (int i = 2; i * i <= value; i++) {
         if (value % i == 0)
@@ -12,6 +12,8 @@ if (value <= 1)
     }
     return true;
 }
+}
+        
 
 uint64_t nPrime(uint64_t n) {
 uint64_t num = 0;
