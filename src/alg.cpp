4d12d2e2a304e948007567811ac2506c7a013cb5
olesyakomarova <olesyakomarova8741@gmail.com>
2023-02-26 14:17:13 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,17 +4,45 @@
 
 
 bool checkPrime(uint64_t value) {
-// вставьте код функции
+if (value <= 1)
+        return false;
+    for (int j = 2; j * j <= value; j++) {
+        if (value % j == 0)
+            return false;
+    }
+    return true;
 }
 
 uint64_t nPrime(uint64_t n) {
-// вставьте код функции
+uint64_t num = 0;
+    uint64_t number = 0;
+    while (n > 0) {
+        if (checkPrime(num) == 1) {
+            number = num;
+            --n;
+        }
+        ++num;
+    }
+    return number;
 }
 
 uint64_t nextPrime(uint64_t value) {
-// вставьте код функции
+while (true) {
+        value++;
+        if (checkPrime(value)) {
+            return value;
+        }
+    }
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-// вставьте код функции
+uint64_t sum = 0;
+    hbound = hbound - 1;
+    while (hbound > 0) {
+        if (checkPrime(hbound)) {
+            sum += hbound;
+        }
+        --hbound;
+    }
+    return sum;
 }
