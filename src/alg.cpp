28870b808d498c9cb05a7c35b1f54f63c971a5b1
olesyakomarova <116936462+olesyakomarova@users.noreply.github.com>
2023-02-26 15:50:48 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -12,9 +12,7 @@ if (value <= 1) {
     }
     return true;
   }
-}
-        
-
+}        
 uint64_t nPrime(uint64_t n) {
 uint64_t num = 0;
     uint64_t number = 0;
@@ -27,7 +25,6 @@ uint64_t num = 0;
     }
     return number;
 }
-
 uint64_t nextPrime(uint64_t value) {
 while (true) {
         value++;
@@ -36,7 +33,6 @@ while (true) {
         }
     }
 }
-
 uint64_t sumPrime(uint64_t hbound) {
 uint64_t sum = 0;
     hbound = hbound - 1;
